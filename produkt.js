35b0efe1adead4e7fb51ccd7bdae5269c5df2645
emno0001 <emno0001@stud.kea.dk>
2025-02-18 15:13:25 +0100

    brødkrummetekst prdokut

--- produkt.js
+++ produkt.js
@@ -20,6 +20,8 @@ let productContainer = document.querySelector(".container");
 fetch(`https://dummyjson.com/recipes/${myProductId}`)
   .then((response) => response.json())
   .then((element) => {
+    document.querySelector("#mealtypebreadcrumb").innerHTML = `<a  href="produktliste.html?mealType=${element.mealType}">${element.mealType}</a> > `;
+    document.querySelector("#namebreadcrumb").textContent = element.name;
     productContainer.innerHTML = `   
         <h1>${element.name}</h1>
 
@@ -30,29 +32,33 @@ fetch(`https://dummyjson.com/recipes/${myProductId}`)
             <img class="produkt_img" src="https://cdn.dummyjson.com/recipe-images/${element.id}.webp" alt="Billede">
         </div>
 
-        <div>
-            <p>Cuisine: ${element.cuisine}</p>
-            <p>Meal type: ${element.mealType}</p>
-            <p>Calories Pr. Serving: ${element.caloriesPerServing}</p>
-            <p>Difficulty: ${element.difficulty}</p>
-            <p>Rating: ${element.rating}</p>
-        </div>
+        <div class="elementer">
+        <p>Cuisine: ${element.cuisine}</p>
+        <p>Meal type: ${element.mealType}</p>
+        <p>Calories Pr. Serving: ${element.caloriesPerServing}</p>
+        <p>Difficulty: ${element.difficulty}</p>
+        <div class="rating">
+                        <p>Rating: ${element.rating}/5.0</p>
+                        <img class="stjerne" src="ikoner/star.svg" alt="stjerne_ratings">
+                    </div>
+    </div>
+        
 
     </div>
 
     <div class="info">
         <div>
-            <p>${element.prepTimeMinutes} <span><img class="ikon" src="ikoner/ur.svg" alt="ur"></span></p>
+            <p>Prep time minutes: ${element.prepTimeMinutes} <span><img class="ikon" src="ikoner/ur.svg" alt="ur"></span></p>
 
         </div>
 
         <div>
-            <p>${element.servings} <span><img class="ikon" src="ikoner/peroner.svg" alt="personer"></span></p>
+            <p>Servings: ${element.servings} <span><img class="ikon" src="ikoner/peroner.svg" alt="personer"></span></p>
 
         </div>
 
         <div>
-            <p>${element.cookTimeMinutes} <span><img class="ikon" src="ikoner/ur.svg" alt="ur"></span></p>
+            <p>Cook time minutes: ${element.cookTimeMinutes} <span><img class="ikon" src="ikoner/ur.svg" alt="ur"></span></p>
 
         </div>
     </div>
